aa77f3bd08aa78c062dbd5ce71d187b881307104
syed <syed@testpress.in>
2024-09-19 11:48:49 +0530

    Add Email backend password reset view password reset done view password reset confirm view password reset complete view Configured the Routes Password change view

--- ForumApp/settings.py
+++ ForumApp/settings.py
@@ -136,3 +136,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
+
+LOGIN_URL = 'login'
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
